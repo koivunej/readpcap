0d35bee3b8f15b6601f3dcbf5dfabc003369555d
Joonas Koivunen <joonas.koivunen@gmail.com>
2017-06-28 16:59:16 +0300

    lib: remove prototype stuff which should not had been commited

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,3 @@ extern crate tokio_io;
 pub mod pcap;
 pub mod ip;
 pub mod tcp;
-
-pub mod tracking {
-    use std::collections::HashMap;
-    use tcp::ConnectionId;
-    use tcp::Connection;
-
-    pub struct NaiveTcpConnectionTracker {
-        connections: HashMap<ConnectionId, Connection>,
-    }
-}
